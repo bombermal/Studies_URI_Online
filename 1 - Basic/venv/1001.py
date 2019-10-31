6ba9de4ad4d65569b3f29faee0f5b5102d250e73
Ivan <ivanalisson@hotmail.com>
2019-10-31 00:30:03 -0300

    Fist commit

---
+++ 1 - Basic/venv/1001.py
@@ -0,0 +1,50 @@
+#1001
+def func1001():
+    a = int(input())
+    b = int(input())
+    X = a+b
+    print("X = "+str(X))
+
+#1002
+def func1002():
+    pi = 3.14159
+    r = float(input())
+    print("A=%.4f" % (pi*r**2))
+
+#1003
+def func1003():
+    a = int(input())
+    b = int(input())
+    SOMA = a+b
+    print("SOMA = "+ str(SOMA))
+
+#1004
+def func1004():
+    a = int(input())
+    b = int(input())
+    print("PROD = "+ str(a*b))
+
+#1005
+def func1005():
+    a = float(input())
+    b = float(input())
+    print("MEDIA = %.5f" %((a*3.5+b*7.5)/11.))
+
+#1006
+def func1006():
+    a = float(input())
+    b = float(input())
+    c = float(input())
+    print("MEDIA = %.1f" %((a*2+b*3+c*5)/10.))
+
+#1007
+def func1007():
+    a  = int(input())
+    b  = int(input())
+    c  = int(input())
+    d  = int(input())
+
+    print("DIFERENCA = %d" %(a*b-c*d))
+
+
+func1007()
---
+++ REadMe.md
@@ -0,0 +1,3 @@
+# URI Online Judge
+
+study
